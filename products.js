d6721aedda6b6b47694dcbfce94af0dfa9aef873
maliktabishooo <maliktabish530@gmail.com>
2026-01-02 00:34:46 +0500

    Update products.js

--- products.js
+++ products.js
@@ -3,21 +3,21 @@ const products = [
     id: 1,
     name: "Luxury Sunglasses",
     price: 4500,
-    image: "assets/images/glasses.jpg",
+    image: "./glasses.jpg",
     category: "Glasses"
   },
   {
     id: 2,
     name: "Premium Watch",
     price: 12000,
-    image: "assets/images/watch.jpg",
+    image: "./watch.jpg",
     category: "Watches"
   },
   {
     id: 3,
-    name: "Designer Outfit",
+    name: "Designer Clothing",
     price: 8500,
-    image: "assets/images/clothes.jpg",
+    image: "./clothes.jpg",
     category: "Clothes"
   }
 ];
